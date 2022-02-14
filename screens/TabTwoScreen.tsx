efdb75a4d1eac3beb16f9f4ed22d5f8a39607868
motoki_kobayashi <motoki.kobayashi0129@gmail.com>
2022-02-14 20:54:15 +0900

    KB-5 add TabTwo flatlist

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -3,10 +3,57 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+import React, { useState } from "react";
+import { FlatList, SafeAreaView, StatusBar, TouchableOpacity } from "react-native";
+
+
+
 export default function TabTwoScreen() {
+  const DATA = [
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      title: "First Item",
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: "Second Item",
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-145571e29d72",
+      title: "Third Item",
+    },
+  ];
+  const [selectedId, setSelectedId] = useState(null);
+  const Item = ({ item, onPress, backgroundColor, textColor }:{item: any, onPress:any, backgroundColor:any, textColor:any}) => (
+    <TouchableOpacity 
+      onPress={onPress}  
+      style={[styles.item, backgroundColor]}>
+      <Text style={[styles.title, textColor]}>{item.title}</Text>
+    </TouchableOpacity>
+  );
+
+  const renderItem = ({ item }:{item:any}) => {
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+    const color = item.id === selectedId ? 'white' : 'black';
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={{ backgroundColor }}
+        textColor={{ color }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        extraData={selectedId}
+      />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
     </View>
@@ -27,5 +74,10 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  item: {
+    padding: 10,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
 });
