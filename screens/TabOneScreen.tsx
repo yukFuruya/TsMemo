90f1365a205abfc96f2271a08d69f9f654cc55b0
yukFuruya <furuya@sasase.ics.keio.ac.jp>
2022-04-19 20:25:01 +0900

    FRY-17: firebase読み込み部分を外出し

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,52 +1,29 @@
 import { ShadowPropTypesIOS, StyleSheet } from "react-native";
 import { FAB } from "react-native-paper";
-import React,{ useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
 import { Calendar } from "react-native-calendars";
-
-import * as firebase from 'firebase';
-import 'firebase/firestore';
-
-if (!firebase.apps.length){
-  const firebaseConfig = {
-    apiKey: "AIzaSyBew5xlRvvVcaceN1eRt2zxbzJVkU_RomI",
-    authDomain: "my-first-tsmemo-native.firebaseapp.com",
-    projectId: "my-first-tsmemo-native",
-    storageBucket: "my-first-tsmemo-native.appspot.com",
-    messagingSenderId: "329406136992",
-    appId: "1:329406136992:web:479e85eefd2d2387c43fc5",
-    measurementId: "G-LPWPD3VH43",
-  };
-  firebase.initializeApp(firebaseConfig);
-}
-
-type User = {
-  age: string;
-  family: string;
-  last: string;
-}
+import { getUsers } from "../lib/firebase";
+import { User } from "../types/user";
 
 export default function TabOneScreen(
   this: any,
   { navigation }: RootTabScreenProps<"TabOne">
 ) {
-
-  const [users,setUsers] = useState<User[]>([])
+  const [users, setUsers] = useState<User[]>([]);
   useEffect(() => {
     getFirebaseItems();
-  },[])
+  }, []);
 
-  const getFirebaseItems = async() => {
-      const snapshot = await firebase.firestore().collection("user").get();
-      const users = snapshot.docs.map(doc => doc.data() as User);
-      console.log(users);
-      setUsers(users);
-  }
+  const getFirebaseItems = async () => {
+    const users = await getUsers();
+    setUsers(users);
+  };
 
   const userItems = users.map((user, index) => (
-    <View style={{margin: 10}} key={index.toString()}>
+    <View style={{ margin: 10 }} key={index.toString()}>
       <Text>{user.age}</Text>
       <Text>{user.family}</Text>
       <Text>{user.last}</Text>
