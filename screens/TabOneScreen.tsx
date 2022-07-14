a4e8d4197cdbb7dc96a9ce295113e071fab6bce6
yukFuruya <furuya@sasase.ics.keio.ac.jp>
2022-07-14 20:39:33 +0900

    カレンダーハイライト表示の対応

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -2,7 +2,7 @@ import { FlatList, ShadowPropTypesIOS, StyleSheet } from "react-native";
 import { getDiarys } from "../lib/firebase";
 import { DiaryItem } from "../components/DiaryItem";
 import { FAB } from "react-native-paper";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { RootStackParamList, RootTabScreenProps } from "../types";
@@ -18,9 +18,12 @@ type Props = {
   navigation: StackNavigationProp<RootStackParamList, "User">;
 };
 
+const INITIAL_DATE = '2022-07-06';
+
 export default function TabOneScreen(this: any, { navigation }: Props) {
   const [users, setUsers] = useState<User[]>([]);
   const [diarys, setDiarys] = useState<Diary[]>([]);
+  const [selected, setSelected] = useState(INITIAL_DATE);
   useEffect(() => {
     getFirebaseItems();
     const fetchDiarys = async () => {
@@ -29,6 +32,25 @@ export default function TabOneScreen(this: any, { navigation }: Props) {
     };
     fetchDiarys();
   }, []);
+
+  const onDayPress = useCallback((day) => {
+    setSelected(day.dateString);
+  }, []);
+
+  const marked = useMemo(() => {
+    return {
+      [selected]: {
+        selected: true,
+        disableTouchEvent: false,
+        selectedColor: 'skyblue',
+        selectedTextColor: 'white'
+      },
+      ['2022-07-22']: {
+        dotColor: 'red',
+        marked: true
+      }
+    };
+  }, [selected]);
 
   const getFirebaseItems = async () => {
     const users = await getUsers();
@@ -52,8 +74,8 @@ export default function TabOneScreen(this: any, { navigation }: Props) {
         ListHeaderComponent={
           <Calendar
             monthFormat={"yyyy年 M月"}
-            onDayPress={this.onDayPress}
-            markingType={"period"}
+            onDayPress={onDayPress}
+            markedDates={marked}
           />
         }
         data={diarys}
